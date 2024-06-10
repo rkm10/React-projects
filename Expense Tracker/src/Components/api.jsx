a722ff2b0bd313fc6c4324c90b3764861e6508ff
rkm10 <rajkumarmalluri08@gmail.com>
2024-06-10 18:06:34 +0530

    sada

--- Expense Tracker/src/Components/api.jsx
+++ Expense Tracker/src/Components/api.jsx
@@ -1,3 +1,8 @@
 import axios from 'axios'
 
 const url = "http://localhost:3000/data";
+
+export async function getData() {
+      const response = await axios.get(url);
+      return response.data;
+}
