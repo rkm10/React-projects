9b3c9eae52a8a1fa35fde7542919a698d5145a8c
rkm10 <rajkumarmalluri08@gmail.com>
2024-07-11 11:01:13 +0530

    card update

--- slot-booking-app/src/components/InvociceCard.jsx
+++ slot-booking-app/src/components/InvociceCard.jsx
@@ -4,7 +4,7 @@ import React from "react";
 
 export default function InvociceCard() {
     return (
-        <Card sx={{ minWidth: 275 }}>
+        <Card sx={{ minWidth: { xs: "100%", md: "100%", lg: "40%" } }}>
             <CardContent>
                 <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
                     SUMMARY
@@ -12,7 +12,31 @@ export default function InvociceCard() {
             </CardContent>
             <CardContent>
                 <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                    SUMMARY
+                    Room Type
+                </Typography>
+                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                    Date
+                </Typography>
+            </CardContent>
+            <CardContent>
+                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                    CUSTOMER
+                </Typography>
+                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                    CUSTOMER NAME
+                </Typography>
+            </CardContent>
+            <CardContent>
+                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                    COST BREAKDOWN
+                </Typography>
+                <CardContent>
+                    Details along with cost
+                </CardContent>
+            </CardContent>
+            <CardContent>
+                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                    Total Price with gst
                 </Typography>
             </CardContent>
 
